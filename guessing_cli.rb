2e920140d9d3b8793ca89544b7ec59288e20933e
jameseclement <jameseclement@gmail.com>
2019-01-01 14:47:23 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,8 +2,7 @@
 
 
 def run_guessing_game
- guess = ""
- until guess == "exit"
+ 
  die_roll = 1 + rand(6)
  puts "Guess a number between 1 and 6."
  guess = gets.chomp.to_s
@@ -12,6 +11,7 @@ def run_guessing_game
    return "You guessed the correct number!"
  elsif guess != die_roll
    return "The computer guessed #{die_roll}"
-end 
+ elsif guess == "exit"
+ return "Goodbye"
 end 
  end
